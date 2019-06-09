c09637964a8483dc6f9700dde66531b35c549b2b
Brennan Douglas <brennand97@gmail.com>
2019-06-09 14:02:12 -0700

    feat(lambdas): finished tanks

---
+++ api/src/routes/tank/index.ts
@@ -0,0 +1,62 @@
+import Boom from 'boom';
+
+import * as appExport from '../../app';
+import { TankController } from '../../controllers/tank';
+import { TankValidator } from '../../controllers/tank/validate';
+import { requireAuthentication } from '../../middleware';
+
+import { ITankController } from '../../controllers/tank/types';
+import { Application, Router, Request, Response, NextFunction } from 'express';
+
+// tslint:disable-next-line:no-require-imports no-var-requires
+const validate = require('express-validation');
+
+const app = <Application>appExport;
+const controller: ITankController = new TankController('tanks');
+
+app.use((req: Request, res: Response, next: NextFunction) => next());
+
+
+// GET
+app.get(
+    '/api/tank/id/:id',
+    async (req, res, next) => controller.getTank(req, res, next)
+);
+app.get(
+    '/api/tank/monitoring',
+    async (req, res, next) => controller.getTankMonitoring(req, res, next)
+);
+app.get(
+    '*/',
+    async (req, res, next) => controller.getTanks(req, res, next)
+);
+
+// POST
+app.post(
+    '/api/tank/',
+    validate(TankValidator.createTank),
+    requireAuthentication,
+    async (req, res, next) => controller.createTank(req, res, next)
+);
+
+// PATCH
+app.patch(
+    '/api/tank/id/:id',
+    validate(TankValidator.updateTank),
+    requireAuthentication,
+    async (req, res, next) => controller.updateTank(req, res, next)
+);
+
+// DELETE
+app.delete(
+    '/api/tank/id/:id',
+    requireAuthentication,
+    async (req, res, next) => controller.deleteTank(req, res, next)
+);
+
+app.use(
+    '*',
+    (req, res) => res.status(404).send(Boom.badRequest(`${req.originalUrl} doesn't exist`))
+);
+
+module.exports = app;
